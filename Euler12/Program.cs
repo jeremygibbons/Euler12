e448e75889c2ec74c3ef9821aebe1d9004132c0c
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-08-28 21:31:33 +0200

    Initial valid solution

---
+++ Euler12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n;
+            uint.TryParse(args[0], out n);
+            uint i = 1;
+            uint p = 1;
+            while(NumDividors(i) < n)
+            {
+                p++;
+                i = NthTriangleNumber(p);
+            }
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+
+        static uint NthTriangleNumber(uint n)
+        {
+            return n * (n + 1) / 2;
+        }
+
+        static int NumDividors(uint n)
+        {
+            SortedSet<uint> results = new SortedSet<uint>();
+            for (uint i = 1; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    results.Add(i);
+                    results.Add(n / i);
+                }
+            }
+            return results.Count;
+        }
+    }
+}
